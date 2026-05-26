00c41c1cce17c8163f9692c48b502219729fdecb
Denis Smirnov <denis.yu.smirnov@mail.ru>
2026-05-26 07:27:09 +0000

    Experiment with polynom-based gelu

--- default/1_gelu_omp/smirnov_denis/gelu_omp.cpp
+++ default/1_gelu_omp/smirnov_denis/gelu_omp.cpp
@@ -5,6 +5,32 @@
 #include <stdio.h>
 #include "gelu_omp.h"
 
+inline float gelu_fast_rational(float x) {
+    float abs_x = std::abs(x);
+
+    const float c1 = 0.200855f;
+    const float c2 = 0.109956f;
+    const float c3 = 0.000390f;
+    const float c4 = 0.020427f;
+
+    float x2 = x * x;
+    float abs_x3 = abs_x * x2;
+    float x4 = x2 * x2;
+    
+    float poly = 1.0f + c1 * abs_x + c2 * x2 + c3 * abs_x3 + c4 * x4;
+
+    float poly2 = poly * poly;
+    float poly4 = poly2 * poly2;
+    float inv_poly4 = 1.0f / poly4;
+
+    float cdf_poly = 0.5f * inv_poly4;
+
+    float sign = std::copysign(1.0f, x);
+    float cdf = 0.5f + sign * (0.5f - cdf_poly);
+    
+    return x * cdf;
+}
+
 inline float fast_tanh(float x) {
     float e2x = std::exp(2.f * x);
     return (e2x - 1.f) / (e2x + 1.f);
@@ -20,14 +46,15 @@ std::vector<float> GeluOMP(const std::vector<float>& input) {
     #pragma omp parallel for
     for (size_t i = 0; i < n; i++) {
         float x = inptr[i];
-        float inner = 0.79788456f * x * (1.f + 0.044715f * x * x);
-        outptr[i] = 0.5f * x * (1.f + fast_tanh(inner));
+        // float inner = 0.79788456f * x * (1.f + 0.044715f * x * x);
+        // outptr[i] = 0.5f * x * (1.f + fast_tanh(inner));
+        outptr[i] = gelu_fast_rational(x);
     }
 
     return output;
 }
 
-#if 0
+#if 1
 std::vector<float> GeluRef(const std::vector<float>& input) {
     size_t n = input.size();
     std::vector<float> output(n);
